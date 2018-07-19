ab94d88a4be0c67e879e6f1613e4d8ecc9a33115
Daniel <danielnimafa@gmail.com>
2018-07-19 09:12:52 +0700

    project setup

---
+++ King Cost/Controller/HomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVC.swift
+//  King Cost
+//
+//  Created by Daniel Nimafa on 17/07/18.
+//  Copyright © 2018 DanielCraft. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ King Cost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  King Cost
+//
+//  Created by Daniel Nimafa on 17/07/18.
+//  Copyright © 2018 DanielCraft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
